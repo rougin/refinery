a129c906428e516f7f92797e66ed9b2eeb5c9659
unknown <rougingutib@gmail.com>
2019-07-17 00:01:15 +0800

    Update MigrateCommandTest

--- tests/Console/MigrateCommandTest.php
+++ tests/Console/MigrateCommandTest.php
@@ -6,7 +6,7 @@ namespace Rougin\Refinery\Console;
  * Migrate Command Test
  *
  * @package Refinery
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class MigrateCommandTest extends AbstractTestCase
 {
