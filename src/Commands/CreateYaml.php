2b85c2be3577630a71708f2b80d29c15a99a3801
Rougin Gutib <rougingutib@gmail.com>
2024-10-16 19:23:28 +0800

    Add "initialize" command

---
+++ src/Commands/CreateYaml.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Rougin\Refinery\Commands;
+
+use Rougin\Blueprint\Commands\InitializeCommand;
+
+/**
+ * @package Refinery
+ *
+ * @author Rougin Gutib <rougingutib@gmail.com>
+ */
+class CreateYaml extends InitializeCommand
+{
+    /**
+     * @var string
+     */
+    protected $file = 'refinery.yml';
+
+    /**
+     * Returns the source directory for the specified file.
+     *
+     * @return string
+     */
+    protected function getPlatePath()
+    {
+        /** @var string */
+        return realpath(__DIR__ . '/../Template');
+    }
+
+    /**
+     * Returns the root directory from the package.
+     *
+     * @return string
+     */
+    protected function getRootPath()
+    {
+        $root = (string) __DIR__ . '/../../../../../';
+
+        $exists = file_exists($root . '/vendor/autoload.php');
+
+        return $exists ? $root : __DIR__ . '/../../';
+    }
+}
---
+++ src/Console.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Rougin\Refinery;
+
+use Rougin\Blueprint\Blueprint;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * @package Refinery
+ *
+ * @author Rougin Gutib <rougingutib@gmail.com>
+ */
+class Console extends Blueprint
+{
+    /**
+     * @var string
+     */
+    protected $name = 'Refinery';
+
+    /**
+     * @var string
+     */
+    protected $file = 'refinery.yml';
+
+    /**
+     * @var string
+     */
+    protected $root;
+
+    /**
+     * @var string
+     */
+    protected $version = '0.4.0';
+
+    /**
+     * @param string $root
+     */
+    public function __construct($root)
+    {
+        $namespace = __NAMESPACE__ . '\Commands';
+
+        $this->setCommandNamespace($namespace);
+
+        $this->setCommandPath(__DIR__ . '/Commands');
+
+        $this->root = $root;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAppPath()
+    {
+        /** @var string */
+        $path = realpath($this->root);
+
+        if (! file_exists($path . '/' . $this->file))
+        {
+            return $path;
+        }
+
+        $parsed = $this->getParsed();
+
+        if (array_key_exists('app_path', $parsed))
+        {
+            /** @var string */
+            $path = $parsed['app_path'];
+        }
+
+        /** @var string */
+        return realpath($path);
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    protected function getParsed()
+    {
+        /** @var string */
+        $path = realpath($this->root);
+
+        if (! file_exists($path . '/' . $this->file))
+        {
+            return array();
+        }
+
+        $file = $path . '/' . $this->file;
+
+        /** @var string */
+        $file = file_get_contents($file);
+
+        // Replace the constant with root path ----
+        $search = '%%CURRENT_DIRECTORY%%';
+
+        $file = str_replace($search, $path, $file);
+        // ----------------------------------------
+
+        /** @var array<string, mixed> */
+        return Yaml::parse($file);
+    }
+}
